7bffde9c78230e6a2f9347ce2d040253c4f3b07e
Harry Bower <BowerHarry@users.noreply.github.com>
2024-01-12 13:16:01 +0000

    more testing

--- RestController.php
+++ RestController.php
@@ -13,19 +13,25 @@ switch ($view) {
 
     case "all":
         // to handle REST Url /mobile/list/
+        echo "GETTING ALL MOBILES";
         $mobileRestHandler = new MobileRestHandler();
         $mobileRestHandler->getAllMobiles();
         break;
 
     case "single":
         // to handle REST Url /mobile/show/<id>/
+        echo "GETTING SOME MOBILES"
         $mobileRestHandler = new MobileRestHandler();
         $mobileRestHandler->getMobile($_GET["id"]);
         break;
 
+    case "test";
+        echo "I HEAR YOU";
+        break;
+
 
     case "":
-        echo "empty request";
+        echo "EMPTY";
         // 404 - not found;
         break;
 }
