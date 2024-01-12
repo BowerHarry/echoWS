5e83558c4185f3bd237117b772037d99cf72f915
Harry Bower <BowerHarry@users.noreply.github.com>
2024-01-12 15:10:17 +0000

    attempt at database connection

---
+++ WSThoughts.php
@@ -0,0 +1,14 @@
+<?php
+
+$serverName = "//bathentrepreneurs.database.windows.net\\sqlexpress";
+
+$connectionInfo = array("Database"=>"bathentrepreneurs", "UID"=> "harrybower","PWD"=> "hA3LLL!aBY8-sR");
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if( $conn ) {
+    echo"Connection established. <br />";
+} else {
+    echo "Connection could not be established. <br />";
+    die(print_r( sqlsrv_errors(), true));
+}
+?>
