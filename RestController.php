ed23de93c8e6de01fa5be490be1284be681906ce
Harry Bower <BowerHarry@users.noreply.github.com>
2024-01-12 13:02:52 +0000

    helloworld v2

--- RestController.php
+++ RestController.php
@@ -1,32 +1,33 @@
 <?php
-require_once ("MobileRestHandler.php");
+echo "hello world";
+// require_once ("MobileRestHandler.php");
 
-$view = "";
-if (isset($_GET["view"]))
-    $view = $_GET["view"];
-/*
- * controls the RESTful services
- * URL mapping
- */
-switch ($view) {
+// $view = "";
+// if (isset($_GET["view"]))
+//     $view = $_GET["view"];
+// /*
+//  * controls the RESTful services
+//  * URL mapping
+//  */
+// switch ($view) {
 
-    case "all":
-        // to handle REST Url /mobile/list/
-        $mobileRestHandler = new MobileRestHandler();
-        $mobileRestHandler->getAllMobiles();
-        break;
+//     case "all":
+//         // to handle REST Url /mobile/list/
+//         $mobileRestHandler = new MobileRestHandler();
+//         $mobileRestHandler->getAllMobiles();
+//         break;
 
-    case "single":
-        // to handle REST Url /mobile/show/<id>/
-        $mobileRestHandler = new MobileRestHandler();
-        $mobileRestHandler->getMobile($_GET["id"]);
-        break;
+//     case "single":
+//         // to handle REST Url /mobile/show/<id>/
+//         $mobileRestHandler = new MobileRestHandler();
+//         $mobileRestHandler->getMobile($_GET["id"]);
+//         break;
 
-    case "helloworld";
-        echo "helloworld";
+//     case "helloworld";
+//         echo "helloworld";
 
-    case "":
-        // 404 - not found;
-        break;
-}
+//     case "":
+//         // 404 - not found;
+//         break;
+// }
 ?>
