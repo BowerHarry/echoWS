6e2ec81c0600ee79b9dd6375599d30ea984c16d8
Harry Bower <BowerHarry@users.noreply.github.com>
2024-01-12 13:27:29 +0000

    switch to single quotes

--- RestController.php
+++ RestController.php
@@ -11,26 +11,26 @@ if (isset($_GET["view"]))
  */
 switch ($view) {
 
-    case "all":
+    case 'all':
         // to handle REST Url /mobile/list/
         echo "GETTING ALL MOBILES";
         $mobileRestHandler = new MobileRestHandler();
         $mobileRestHandler->getAllMobiles();
         break;
 
-    case "single":
+    case 'single':
         // to handle REST Url /mobile/show/<id>/
         echo "GETTING SOME MOBILES";
         $mobileRestHandler = new MobileRestHandler();
         $mobileRestHandler->getMobile($_GET["id"]);
         break;
 
-    case "test";
+    case 'test';
         echo "I HEAR YOU";
         break;
 
 
-    case "":
+    case '':
         echo "EMPTY";
         // 404 - not found;
         break;
