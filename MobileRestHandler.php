c39781161ee2c28bb2b3a79a06e0c99ea3f3201a
Harry Bower <BowerHarry@users.noreply.github.com>
2024-01-12 13:43:00 +0000

    removed encoding options

--- MobileRestHandler.php
+++ MobileRestHandler.php
@@ -20,48 +20,15 @@ class MobileRestHandler extends SimpleRest
             $statusCode = 200;
         }
 
-        $requestContentType = $_SERVER['HTTP_ACCEPT'];
-        $this->setHttpHeaders($requestContentType, $statusCode);
-
-        if (strpos($requestContentType, 'application/json') !== false) {
-            echo "json";
-            $response = $this->encodeJson($rawData);
-            echo $response;
-        } else if (strpos($requestContentType, 'text/html') !== false) {
-            echo "html";
-            $response = $this->encodeHtml($rawData);
-            echo $response;
-        } else if (strpos($requestContentType, 'application/xml') !== false) {
-            echo "xml";
-            $response = $this->encodeXml($rawData);
-            echo $response;
-        }
-    }
-
-    public function encodeHtml($responseData)
-    {
-        $htmlResponse = "<table border='1'>";
-        foreach ($responseData as $key => $value) {
-            $htmlResponse .= "<tr><td>" . $key . "</td><td>" . $value . "</td></tr>";
-        }
-        $htmlResponse .= "</table>";
-        return $htmlResponse;
+        echo "json";
+        $response = $this->encodeJson($rawData);
+        echo $response;
     }
 
     public function encodeJson($responseData)
     {
         $jsonResponse = json_encode($responseData);
         return $jsonResponse;
-    }
-
-    public function encodeXml($responseData)
-    {
-        // creating object of SimpleXMLElement
-        $xml = new SimpleXMLElement('<?xml version="1.0"?><mobile></mobile>');
-        foreach ($responseData as $key => $value) {
-            $xml->addChild($key, $value);
-        }
-        return $xml->asXML();
     }
 
     public function getMobile($id)
@@ -80,17 +47,8 @@ class MobileRestHandler extends SimpleRest
 
         $requestContentType = $_SERVER['HTTP_ACCEPT'];
         $this->setHttpHeaders($requestContentType, $statusCode);
-
-        if (strpos($requestContentType, 'application/json') !== false) {
-            $response = $this->encodeJson($rawData);
-            echo $response;
-        } else if (strpos($requestContentType, 'text/html') !== false) {
-            $response = $this->encodeHtml($rawData);
-            echo $response;
-        } else if (strpos($requestContentType, 'application/xml') !== false) {
-            $response = $this->encodeXml($rawData);
-            echo $response;
-        }
+        $response = $this->encodeJson($rawData);
+        echo $response;
     }
 }
 ?>
